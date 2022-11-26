9bb84118cd6095765977ca78dc5fe6c56390d82e
MatthiasDobiosz <matthido98@web.de>
2022-11-26 11:43:53 +0100

    added prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 interface Leo {
   name: string;
 }
 
 function App() {
-
-  const [test, setTest] = useState<Leo>({name: 'hey'});
+  const [test, setTest] = useState<Leo>({ name: "hey" });
 
   useEffect(() => {
-    console.log("hey")
-    fetch('/test').then(res => res.json()).then(data => setTest({name: data.message}));
-  })
+    console.log("hey");
+    fetch("/test")
+      .then((res) => res.json())
+      .then((data) => setTest({ name: data.message }));
+  });
+  let i = 1;
 
-  return (
-    <div>
-      {test.name}
-    </div>
-  )
+  return <div>{test.name}</div>;
 }
 
-export default App
+export default App;
