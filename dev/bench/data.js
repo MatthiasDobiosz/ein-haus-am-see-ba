7f5e616b18323cda86673e5140130dcc7cd623a6
github-action-benchmark <github@users.noreply.github.com>
2022-12-02 17:50:41 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 6729f0a31d20e84988ed7a958932460ce76d3c73

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1670003441847,
+  "repoUrl": "https://github.com/MatthiasDobiosz/ein-haus-am-see-ba",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "matthido98@web.de",
+            "name": "MatthiasDobiosz",
+            "username": "MatthiasDobiosz"
+          },
+          "committer": {
+            "email": "matthido98@web.de",
+            "name": "MatthiasDobiosz",
+            "username": "MatthiasDobiosz"
+          },
+          "distinct": true,
+          "id": "6729f0a31d20e84988ed7a958932460ce76d3c73",
+          "message": "outputfile",
+          "timestamp": "2022-12-02T18:49:47+01:00",
+          "tree_id": "a201437055f5373e0aaf8a0d39fca466b5a5932d",
+          "url": "https://github.com/MatthiasDobiosz/ein-haus-am-see-ba/commit/6729f0a31d20e84988ed7a958932460ce76d3c73"
+        },
+        "date": 1670003440701,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Test1",
+            "value": 31840313,
+            "range": "±1.76%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
