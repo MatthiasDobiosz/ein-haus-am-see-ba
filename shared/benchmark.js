d7b40134e9eda71c6d43723d4f669def01c79208
MatthiasDobiosz <matthido98@web.de>
2022-12-03 10:19:51 +0100

    test function

--- shared/benchmark.js
+++ shared/benchmark.js
@@ -12,4 +12,12 @@ suite
   .on("complete", () => {
     console.log("Done with the test");
   })
+  .add("for-schleife", () => {
+    for (let i = 0; i < 100000; i++) {
+      i++;
+    }
+  })
+  .on("cycle", (event) => {
+    console.log(String(event.target));
+  })
   .run();
